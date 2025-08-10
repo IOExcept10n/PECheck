147165486ef5b231986ea9a63d75902c8ae0eb57
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2025-08-10 10:55:39 +0500

    add

--- src/backend/api/Normative.cs
+++ src/backend/api/Normative.cs
@@ -3,7 +3,7 @@ namespace api.Models
     public class Normative
     {
         public string Name { get; set; }
-        public string Schedule { get; set; }
+        public string Description { get; set; }
         public List<int> Students { get; set; }
         public List<string> Teachers { get; set; }
         public int MaxStudents { get; set; }
